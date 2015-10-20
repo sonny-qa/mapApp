1264ff5524987b3ca9e8c1625f9db29277f1a212
sonny parsonage <sonnyparsonage@gmail.com>
2015-10-19 17:14:26 -0700

    added a button and lat long input fields

--- public/js/gservice.js
+++ public/js/gservice.js
@@ -24,11 +24,20 @@ angular.module('gservice', [])
 
 			}
 
+		//set inital marker location using Latlng class
+		var initiallocation = new google.maps.LatLng(latitiude,longitude);
+		
+		//create a new point using this latlng class instance, using a marker instance
+		var marker = new google.maps.Marker({
+			position: initiallocation,
+			map: map
+		})
+			
 
 		}
 
 		
-
+		//listener so that map loads on page load
 		google.maps.event.addDomListener(window, 'load',
 			initialize(selectedLat,selectedLong))
 		return googleMapService;
